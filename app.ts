8c8e7233a439fdb8a5e9ede5ab0ada694f2855a6
PietroFDEV <pi_favoreto@yahoo.com.br>
2024-03-03 21:45:07 -0300

    Desafio DIO
    Finalizado desafio com desenvolvimento solicitados

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { VipAccount } from './class/VipAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(100)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(200)
 console.log(companyAccount)
+const vipAccount: VipAccount = new VipAccount('VIP', 30)
+vipAccount.deposit(200)
+console.log(vipAccount)
